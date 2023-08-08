b9b6f766e1fc61b048a3aad63e2394033dc8ab5f
Kalyanidosapati <116077159+Kalyanidosapati@users.noreply.github.com>
2023-08-08 17:06:26 +0530

    Update SpringBootDockerApplication.java

--- src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Welcome to springboot..!!";
+		return "Welcome to springBoot Application..!!";
 	}
 
 	public static void main(String[] args) {
